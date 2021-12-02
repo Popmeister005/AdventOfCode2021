46b236f102518ee872ca7c9d47c50780f193bba4
Popmeister005 <75345936+Popmeister005@users.noreply.github.com>
2021-12-01 22:54:58 -0500

    setting up repo

---
+++ Day1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static Utilities;
+
+namespace Day1
+{
+    class Day1
+    {
+        static void Main(string[] args)
+        {
+            //string[] lines = ReadPuzzleInput("TestInput.txt");
+            string[] lines = ReadPuzzleInput("PuzzleInput.txt");
+
+            int increaseCount = 0;
+            int previousDepth = 0;
+            int slidingcount = 0;
+
+            for(int i = 0; i < (lines.Length-2); i++)
+            {
+                slidingcount++;
+
+                int currentDepth = Int16.Parse(lines[i]) + Int16.Parse(lines[i + 1]) + Int16.Parse(lines[i + 2]);
+
+                if (currentDepth > previousDepth)
+                    {
+                    increaseCount++;
+                    }
+
+                previousDepth = currentDepth;
+            }
+
+            Console.WriteLine(increaseCount - 1);
+        }
+    }
+}
